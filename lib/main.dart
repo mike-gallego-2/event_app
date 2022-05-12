ae4950344657f15e6665de28f9374a51dd3da778
mike-gallego-2 <mgallego.business@gmail.com>
2022-05-12 08:17:35 -0400

    :zap: initialize firebase into app

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:event_app/repositories/map_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: 'credentials.env');
   runApp(const EventApp());
 }
